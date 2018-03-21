75063a0586e39ab50b6bf8518acde50e90985b29
Michał Szyszka <szyszka.michal@gmail.com>
2018-03-21 21:02:18 +0100

    początek wersji 0.1

--- InvertedIndex/InvertedIndex.cpp
+++ InvertedIndex/InvertedIndex.cpp
@@ -2,10 +2,22 @@
 //
 
 #include "stdafx.h"
+#include <string>
+#include <iostream>
 
-
-int main()
+int main(int argc, char** argv)
 {
+	if (argc == 1) {
+		// createIndex
+	}
+	else {
+		std::cout << "Usage: InvertedIndex <index file> [<list file>]" << std::endl;
+		return 1;
+	}
+	for (std::string word; std::getline(std::cin, word); ) {
+		// search(word)
+		std::cout << word << std::endl;
+	}
     return 0;
 }
 
